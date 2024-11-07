fdc06769658d7ff917bba5c253d41b6b6238093e
SIMIC <ZELJKO.SIMIC@FISGLOBAL.COM>
2024-11-07 11:33:51 +0100

    Add secureui microservice

---
+++ secureui/src/main/java/com/pluralsight/secureui/ReportController.java
@@ -0,0 +1,19 @@
+package com.pluralsight.secureui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReportController {
+
+	@RequestMapping("/")
+	public String loadHome() {
+		return "home";
+	}
+	
+	@RequestMapping("/report")
+	public String loadReport(Model m) {
+		return "report";
+	}
+}
---
+++ secureui/src/main/java/com/pluralsight/secureui/SecureuiApplication.java
@@ -0,0 +1,14 @@
+package com.pluralsight.secureui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SecureuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureuiApplication.class, args);
+	}
+
+}
