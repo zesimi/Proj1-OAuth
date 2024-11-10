98ff221e68a5883cd67dae571bb2e959319de4f8
Someuser1 <some564em646@gmail.com>
2024-11-10 13:13:15 +0100

    Adding resourceserver1.

---
+++ resourceserver1/src/main/java/com/example/resourceserver1/Resourceserver1Application.java
@@ -0,0 +1,13 @@
+package com.example.resourceserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resourceserver1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resourceserver1Application.class, args);
+	}
+
+}
---
+++ resourceserver1/src/test/java/com/example/resourceserver1/Resourceserver1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resourceserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resourceserver1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
